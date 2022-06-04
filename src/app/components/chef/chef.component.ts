672933dfaff418d0e950f097e841d87e0572187a
jliq <jliq198679@gmail.com>
2022-06-04 17:59:46 -0400

    Configuracion de chef

--- src/app/components/chef/chef.component.ts
+++ src/app/components/chef/chef.component.ts
@@ -1,3 +1,8 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PayloadWebService, UploadService } from './../../services';
+import { FrameWebIdEnum } from './../../enums';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { defaultImg } from './../../tools';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChefComponent implements OnInit {
 
-  constructor() { }
+  chefImage: string | ArrayBuffer = defaultImg;
+  form: FormGroup;
+  selectedFiles
+  frameWeb: FrameWebIdEnum = FrameWebIdEnum.CHEF;
+
+  constructor(
+    private fb: FormBuilder,
+    private payloadWebService: PayloadWebService,
+    private snackBar: MatSnackBar,
+    private uploadService: UploadService
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      description_es: [null, [Validators.required]],
+      description_en: [null, [Validators.required]],
+      image: [null],
+    });
+
+    this.payloadWebService.get(this.frameWeb).subscribe(resp=>{
+     const data = resp.payload_frame;
+      this.form.get('description_es').setValue(data.description_es);
+      this.form.get('description_en').setValue(data.description_en);
+
+      if(data.image) {
+        this.chefImage = data.image;
+      }
+    })
+
   }
 
+  selectFile(event) {
+    const fileReader = new FileReader();
+    this.selectedFiles = event.target.files;
+
+    fileReader.onload = () => {
+      return this.chefImage = fileReader.result;
+    };
+
+    fileReader.readAsDataURL(this.selectedFiles[0]);
+  }
+
+  update() {
+    let data = {
+      description_es: this.form.value.description_es,
+      description_en: this.form.value.description_en
+    };
+
+    if(this.selectedFiles) {
+      this.uploadService.upload(this.selectedFiles[0]).subscribe(imageUrl=>{
+        data['image'] = imageUrl;
+        this.updateRequest(data);
+      })
+    }
+    else {
+      this.updateRequest(data);
+    }
+  }
+
+  updateRequest(data) {
+    this.payloadWebService.update(this.frameWeb, data).subscribe(resp=>{
+      const msg = `Información del chef actualizada de forma correcta`;
+      this.snackBar.open(msg, 'X');
+    })
+  }
 }
